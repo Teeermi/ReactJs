e882f5a16992e50abf6a8420ddae052d02f9ba16
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-11 17:27:49 +0200

    Project Travel List v2

--- State, Events, and Forms Interactive Components/travel-list/src/App.js
+++ State, Events, and Forms Interactive Components/travel-list/src/App.js
@@ -65,14 +65,38 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItem;
+
+  if (sortBy === "input") sortedItem = items;
+
+  if (sortBy === "description") {
+    sortedItem = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItem = items.slice().sort((a, b) => +a.packed - +b.packed);
+  }
+
   return (
-    <div className="list">
-      <ul>
-        {items.map((element) => {
-          return <Item element={element} key={element.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />;
-        })}
-      </ul>
-    </div>
+    <>
+      <div className="list">
+        <ul>
+          {sortedItem.map((element) => {
+            return <Item element={element} key={element.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />;
+          })}
+        </ul>
+
+        <div className="actions">
+          <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+            <option value={"input"}>Sort by the input</option>
+            <option value={"description"}>by description</option>
+            <option value={"packed"}>by packed</option>
+          </select>
+        </div>
+      </div>
+    </>
   );
 }
 
