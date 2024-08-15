278e7732e13ae1e867a2db2238cc07d65b667e63
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-15 08:25:43 +0200

    Project Eat-'N-Split

--- Practice Project Eat-'N-Split/eat-n-split/src/App.js
+++ Practice Project Eat-'N-Split/eat-n-split/src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -49,7 +52,50 @@ function Friend({ friend }) {
       {friend.balance > 0 && <p className="green">Own you a money</p>}
       {friend.balance === 0 && <p className="grey">Even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👤Friend name</label>
+      <input type="text"></input>
+
+      <label>📷 Image url</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💷 Bill value</label>
+      <input type="text" />
+
+      <label>💷 Your cost</label>
+      <input type="text" />
+
+      <label>💷 X</label>
+      <input type="text" disabled />
+
+      <label>💰 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Add</Button>
+    </form>
   );
 }
