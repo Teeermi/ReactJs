86d4eb8a69d28eea8c2d3954af214a2c8edd09a2
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-15 14:20:37 +0200

    Project Eat-'N- Split

--- Practice Project Eat-'N-Split/eat-n-split/src/App.js
+++ Practice Project Eat-'N-Split/eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,12 +22,18 @@ const initialFriends = [
 ];
 
 export default function App() {
+  const [showForm, setShowForm] = useState(false);
+
+  function handlerShowForm() {
+    setShowForm((e) => !e);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showForm && <FormAddFriend />}
+        <Button onClick={handlerShowForm}>{showForm ? "Close" : "Add friend"}</Button>
       </div>
       <FormSplitBill />
     </div>
@@ -57,8 +65,12 @@ function Friend({ friend }) {
   );
 }
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 function FormAddFriend() {
