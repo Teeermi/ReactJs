e852d77673055e168b2cad3e4f8be1f0c0b3c11b
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-15 16:24:49 +0200

    Project Eat-'N-Split

--- Practice Project Eat-'N-Split/eat-n-split/src/App.js
+++ Practice Project Eat-'N-Split/eat-n-split/src/App.js
@@ -125,21 +125,25 @@ function FormAddFriend({ handleAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💷 Bill value</label>
-      <input type="text" />
+      <input type="text" value={bill} onChange={(e) => setBill(+e.target.value)} />
 
       <label>💷 Your cost</label>
-      <input type="text" />
+      <input type="text" value={paidByUser} onChange={(e) => setPaidByUser(+e.target.value > bill ? paidByUser : +e.target.value)} />
 
       <label>💷 {selectedFriend.name} expenses</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={bill - paidByUser} />
 
       <label>💰 Who is paying the bill</label>
-      <select>
+      <select value={whoIsPaying} onChange={(e) => setWhoIsPaying(e.target.value)}>
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
