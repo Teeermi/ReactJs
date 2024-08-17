ec83a7a62f376dce4a1a8f59e953e77c4c1997b0
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-17 20:56:43 +0200

    Project Use Popcorn

--- Thinking in React Components, Composition, and Reusability/Project usePopcorn/use-popcorn/src/StarRating.js
+++ Thinking in React Components, Composition, and Reusability/Project usePopcorn/use-popcorn/src/StarRating.js
@@ -17,6 +17,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTepmRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -26,10 +27,15 @@ export default function StarRating({ maxRating = 5 }) {
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star onClick={() => handleRating(i + 1)} full={i + 1 <= rating} />
+          <Star
+            onClick={() => handleRating(i + 1)}
+            full={tempRating ? i + 1 <= tempRating : i + 1 <= rating}
+            onMouseEnter={() => setTepmRating(i + 1)}
+            onMouseLeave={() => setTepmRating(0)}
+          />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating ? tempRating : rating || ""}</p>
     </div>
   );
 }
@@ -41,9 +47,9 @@ const starStyle = {
   cursor: "pointer",
 };
 
-function Star({ onClick, full }) {
+function Star({ onClick, full, onMouseEnter, onMouseLeave }) {
   return (
-    <span role="button" style={starStyle} onClick={onClick}>
+    <span role="button" style={starStyle} onClick={onClick} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       {full ? (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#000" stroke="#000">
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
