274036248dcdc9ea739fb2e02af0264172162ab3
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-25 19:20:49 +0200

    Project reactQuiz

--- The Advanced useReducer Hook/react-quiz/src/App.js
+++ The Advanced useReducer Hook/react-quiz/src/App.js
@@ -1,7 +1,42 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("ERROR");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:8000/questiasdons");
+        const data = await res.json();
+
+        dispatch({ type: "dataRecived", payload: data });
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
