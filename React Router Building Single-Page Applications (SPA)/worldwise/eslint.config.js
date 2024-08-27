d0bd67916e2be30d3822707ad410e78a677c572e
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-27 21:29:50 +0200

    Project worldWise

--- React Router Building Single-Page Applications (SPA)/worldwise/eslint.config.js
+++ React Router Building Single-Page Applications (SPA)/worldwise/eslint.config.js
@@ -5,6 +5,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
+  { ignores: ["dist"] },
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
@@ -18,10 +19,7 @@ export default [
     },
     settings: { react: { version: "18.3" } },
     plugins: {
-      react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
+
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
