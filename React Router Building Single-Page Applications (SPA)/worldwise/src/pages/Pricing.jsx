70f4fcb88f632819e795a6e690bd6594d8b77d0e
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-28 19:58:35 +0200

    Project worldWise

--- React Router Building Single-Page Applications (SPA)/worldwise/src/pages/Pricing.jsx
+++ React Router Building Single-Page Applications (SPA)/worldwise/src/pages/Pricing.jsx
@@ -1,5 +1,11 @@
+import PageNav from "../components/PageNav";
+
 function Pricing() {
-  return <div>Pricing</div>;
+  return (
+    <div>
+      <PageNav />
+    </div>
+  );
 }
 
 export default Pricing;
--- React Router Building Single-Page Applications (SPA)/worldwise/src/pages/Product.jsx
+++ React Router Building Single-Page Applications (SPA)/worldwise/src/pages/Product.jsx
@@ -1,5 +1,11 @@
+import PageNav from "../components/PageNav";
+
 function Product() {
-  return <div>Product</div>;
+  return (
+    <div>
+      <PageNav />
+    </div>
+  );
 }
 
 export default Product;
