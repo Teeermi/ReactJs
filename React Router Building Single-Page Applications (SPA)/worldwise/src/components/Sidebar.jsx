77afeb10016591aeebde5edde0210603bffcbdc7
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-08-29 20:18:58 +0200

    Project worldWise

---
+++ React Router Building Single-Page Applications (SPA)/worldwise/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />;
+      <AppNav />
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>&copy; {new Date().getFullYear()} World wise</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
