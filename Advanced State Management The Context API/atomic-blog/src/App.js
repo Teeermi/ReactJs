480913e93d596cc4e1fdcc414dc2158dae6075ac
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-09-03 20:57:20 +0200

    Project AtomicBlog

--- Advanced State Management The Context API/atomic-blog/src/App.js
+++ Advanced State Management The Context API/atomic-blog/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -8,22 +8,15 @@ function createRandomPost() {
   };
 }
 
+const PostContext = createContext();
+
 function App() {
-  const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
-  );
+  const [posts, setPosts] = useState(() => Array.from({ length: 30 }, () => createRandomPost()));
   const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
 
   // Derived state. These are the posts that will actually be displayed
-  const searchedPosts =
-    searchQuery.length > 0
-      ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
-        )
-      : posts;
+  const searchedPosts = searchQuery.length > 0 ? posts.filter((post) => `${post.title} ${post.body}`.toLowerCase().includes(searchQuery.toLowerCase())) : posts;
 
   function handleAddPost(post) {
     setPosts((posts) => [post, ...posts]);
@@ -43,19 +36,11 @@ function App() {
 
   return (
     <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
+      <button onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)} className="btn-fake-dark-mode">
         {isFakeDark ? "☀️" : "🌙"}
       </button>
 
-      <Header
-        posts={searchedPosts}
-        onClearPosts={handleClearPosts}
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      />
+      <Header posts={searchedPosts} onClearPosts={handleClearPosts} searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
       <Archive onAddPost={handleAddPost} />
       <Footer />
@@ -71,10 +56,7 @@ function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
       </h1>
       <div>
         <Results posts={posts} />
-        <SearchPosts
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-        />
+        <SearchPosts searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
         <button onClick={onClearPosts}>Clear posts</button>
       </div>
     </header>
@@ -82,13 +64,7 @@ function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
 }
 
 function SearchPosts({ searchQuery, setSearchQuery }) {
-  return (
-    <input
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-      placeholder="Search posts..."
-    />
-  );
+  return <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search posts..." />;
 }
 
 function Results({ posts }) {
@@ -126,16 +102,8 @@ function FormAddPost({ onAddPost }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Post title"
-      />
-      <textarea
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
-        placeholder="Post body"
-      />
+      <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Post title" />
+      <textarea value={body} onChange={(e) => setBody(e.target.value)} placeholder="Post body" />
       <button>Add post</button>
     </form>
   );
@@ -166,9 +134,7 @@ function Archive({ onAddPost }) {
   return (
     <aside>
       <h2>Post archive</h2>
-      <button onClick={() => setShowArchive((s) => !s)}>
-        {showArchive ? "Hide archive posts" : "Show archive posts"}
-      </button>
+      <button onClick={() => setShowArchive((s) => !s)}>{showArchive ? "Hide archive posts" : "Show archive posts"}</button>
 
       {showArchive && (
         <ul>
