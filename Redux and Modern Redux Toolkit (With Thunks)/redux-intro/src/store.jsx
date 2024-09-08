291ecd04e2a2c6049882d7e497880e3158e06143
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-09-08 14:22:04 +0200

    Redux Intro

--- Redux and Modern Redux Toolkit (With Thunks)/redux-intro/src/store.jsx
+++ Redux and Modern Redux Toolkit (With Thunks)/redux-intro/src/store.jsx
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -13,7 +15,7 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return { ...state, loan: action.payload.amount, loanPuropse: action.payload.purpose, balance: state.balance + action.payload.amount };
 
     case "account/payLoan":
       return { ...state, loan: 0, loanPuropse: "", balance: state.balance - state.loan };
@@ -21,3 +23,13 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+
+store.dispatch({ type: "account/requestLoan", payload: { amount: 1000, purpose: "To buy car" } });
+
+store.dispatch({ type: "account/payLoan" });
+
+console.log(store.getState());
