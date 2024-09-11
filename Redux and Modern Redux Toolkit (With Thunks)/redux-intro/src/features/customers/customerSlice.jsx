3233d4624bff8db3e52ebaf3ac6348cf28ff36b5
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-09-11 21:48:34 +0200

    Redux Intro

--- Redux and Modern Redux Toolkit (With Thunks)/redux-intro/src/features/customers/customerSlice.jsx
+++ Redux and Modern Redux Toolkit (With Thunks)/redux-intro/src/features/customers/customerSlice.jsx
@@ -1,24 +1,42 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return { ...state, ...action.payload };
-    case "customer/updateName":
-      return { ...state, fullName: action.payload };
-    default:
-      return state;
-  }
-}
+const customerSlice = createSlice({
+  name: "customer",
+  initialState: initialState,
+  reducers: {
+    createCustomer(state, action) {
+      return { ...state, ...action.payload, createdAt: new Date().toISOString() };
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-export function createCustomer(fullName, nationalID) {
-  return { type: "customer/createCustomer", payload: { fullName, nationalID, createdAt: new Date().toISOString() } };
-}
+// export default function customerReducer(state = initialStateCustomer, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return { ...state, ...action.payload };
+//     case "customer/updateName":
+//       return { ...state, fullName: action.payload };
+//     default:
+//       return state;
+//   }
+// }
 
-export function updateName(fullName) {
-  return { type: "customer/updateName", payload: fullName };
-}
+// export function createCustomer(fullName, nationalID) {
+//   return { type: "customer/createCustomer", payload: { fullName, nationalID, createdAt: new Date().toISOString() } };
+// }
+
+// export function updateName(fullName) {
+//   return { type: "customer/updateName", payload: fullName };
+// }
+
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
