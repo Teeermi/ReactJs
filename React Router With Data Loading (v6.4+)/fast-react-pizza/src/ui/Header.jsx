f84edc3e4aab5f712f5b092e02402f92f77a3519
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-09-14 18:28:30 +0200

    Project Fast React Pizza

---
+++ React Router With Data Loading (v6.4+)/fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Home</Link>
+      <p>Matthew</p>
+    </header>
+  );
+}
+
+export default Header;
